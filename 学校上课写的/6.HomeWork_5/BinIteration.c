4a124f38500c4ac3a5c80ed2429ec85246b3de39
Yaosanqi137 <zxc18023571263@outlook.com>
2024-10-21 20:30:29 +0800

    删除 BinIteration.c 冗余函数

--- 学校上课写的/6.HomeWork_5/BinIteration.c
+++ 学校上课写的/6.HomeWork_5/BinIteration.c
@@ -5,10 +5,6 @@ double f(double x){
     return 2*(x*x*x) - 4*(x*x) + 3*x - 6;
 }
 
-double f1(double x){
-    return 6*(x*x) - 8*x + 3;
-}
-
 double binIteration(double left, double right){ // 原理看书
     if(f(left) * f(right) < 0){
         double mid;
