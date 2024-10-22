4e104d85c5c865c9494d95102fc220b949419fda
Yaosanqi137 <zxc18023571263@outlook.com>
2024-10-22 10:47:03 +0800

    完数更新

--- 学校上课写的/6.HomeWork_5/PerfectNum.c
+++ 学校上课写的/6.HomeWork_5/PerfectNum.c
@@ -2,6 +2,15 @@
 #include <stdio.h>
 #include <time.h>
 
+void factors(int num){
+    int i = 1;
+    do{
+        if (num % i == 0)
+            printf("%d ", i);
+        i++;
+    } while (i < num);
+}
+
 int factorSum(int num){
     int sum = 1;
     for(int i = 2;i * i <= num;i += 2)
@@ -14,14 +23,18 @@ int factorSum(int num){
 }
 
 int main(){
-    int n, start, end;
+    int n, start, end, count = 0;
     printf("type in a number:");
     scanf("%d", &n);
-    printf("its factors are:");
     start = clock();
     for(int j = 1;j <= n;j++)
-        if(j == factorSum(j))
-            printf("%d ", j);
+        if(j == factorSum(j)){
+            printf("%d its factors are: ", j);
+            factors(j);
+            putchar('\n');
+            count++;
+        }
+    printf("there are %d perfect numbers in total within %d", count, n);
     end = clock();
     printf("\ntime:%dms\n", end - start);
     return 0;
