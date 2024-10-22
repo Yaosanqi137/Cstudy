7ca9d697e790e9c00cd33a313453ef09730cc591
Yaosanqi137 <99163721+Yaosanqi137@users.noreply.github.com>
2024-10-22 10:06:04 +0800

    Update BetterPrime.c

--- 学校上课写的/9.HomeWork_6/BetterPrime.c
+++ 学校上课写的/9.HomeWork_6/BetterPrime.c
@@ -19,7 +19,7 @@ int main(){
     for(i = 3;i <= N;i += 2)
         if(!arr[i])
             count++;
-    printf("100000000以内的素数有: %d个\n", count);
-    printf("耗时: %dms\n", end - start);
+    printf("100000000浠ュ唴鐨勭礌鏁版湁: %d涓猏n", count);
+    printf("鑰楁椂: %dms\n", end - start);
     return 0;
 }
