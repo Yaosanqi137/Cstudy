1242f41f5c055e65192dd84ea74eb8edc11d8ad1
Yaosanqi137 <zxc18023571263@outlook.com>
2024-12-10 23:05:32 +0800

    完成词法分析部分

--- 学校上课写的/19.C语言程序设计任务/DuplicateChecking.c
+++ 学校上课写的/19.C语言程序设计任务/DuplicateChecking.c
@@ -1,33 +1,29 @@
-// 参照的C语言标准: C99
-// 引用的头文件
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <math.h>
+#include <ctype.h>
 
 // 定义的宏
 #define MAX_LEN 4096     // 二元组最大长度
 #define SEN_LEN 256      // 最长句长
 #define FILES 2          // 总文件数量
 #define ALL_KEYWORD 37   // 所有关键词的数量
-#define ALL_SYMBOL 27    // 所有运算符、界限符和逻辑符
+#define ALL_SYMBOL 24    // 所有运算符、界限符和逻辑符
 #define ALL_COMMAND 13   // 所有预编译指令
-#define NUMBER 10        // 所有数字
 #define KEYWORD_CODE 1   // 关键字的种别码起始处
 #define SYMBOL_CODE 38   // 各种符号的种别码起始处
-#define NUMBER_CODE 65   // 数字的种别码起始处
-#define COMMAND_CODE 75  // 预编译指令的种别码起始处
-#define STRING_CODE 88   // 字符串种别码
-#define STRUCT_CODE 89   // 标识符种别码
-#define OTHER_CODE 93    // 预编译指令或其他的标识符种别码
+#define COMMAND_CODE 62  // 预编译指令种别码起始处
+#define NUMBER_CODE 75   // 数字的种别码起始处
+#define STRING_CODE 76   // 字符串种别码
+#define IDEN_CODE 77     // 标识符种别码
 
 /* 二元组种别码分配
  * 1 ~ 37  - 关键字
- * 38 ~ 64 - 各种符号
- * 65 ~ 74 - 数字
- * 75 ~ 87 - 预编译指令
- * 88 ------ 字符串标记
- * 89 ------ 标识符
+ * 38 ~ 61 - 各种符号
+ * 62 ~ 74 - 预编译指令
+ * 75 ------ 数字
+ * 76 ------ 字符串标记
+ * 77 ------ 标识符
  * 0 ------- 向量终止符
  * 同时，使用使用二元组进行存储
  * e.g. int main(){printf("helloworld"); return 0;}
@@ -40,9 +36,9 @@ const char *DATAFILE = "Data.c";
 const char *CHECKFILE = "TestCode.c";
 
 // 用于存储所有运算符和分界符
-char *SYMBOLS[] = {"+", "-", "*", "/", "%", "=", "<", ">", ">=", "<=",
-                    "!=", "==", ";", ":", "[", "]", "{", "}", "(", ")",
-                   "&", "|", ",", "'", "\"", "!", ".",
+char *SYMBOLS[] = {"+", "-", "*", "/", "%", "=", "<", ">",
+                   ";", ":", "[", "]", "{", "}", "(", ")",
+                   "&", "|", ",", "'", "\"", "!", ".", "#"
                     };
 
 // 用于存储所有关键字
@@ -56,78 +52,152 @@ char *KEYWORDS[] = {"float", "double", "char", "int", "long", "short",
 
 // 用于存储所有预编译指令
 char *COMMANDS[] = {"if", "endif", "include", "define", "error", "else", "ifdef",
-                      "ifndef", "undef", "line", "include_next", "pragma", "warning"
-                    };
+                    "ifndef", "undef", "line", "include_next", "pragma", "warning"
+};
 
-// 缓冲区
-char *buffer;
+// 单句缓冲区
+char BUFFER[SEN_LEN];
 
-// 单字最长长度
-char *word_A, *word_B, *word_C;
+// 单字缓冲区
+char WORD[SEN_LEN];
 
 // 用于计算的二元组
 typedef struct {
     int code[MAX_LEN];
-    char str[MAX_LEN];
+    char str[MAX_LEN][SEN_LEN];
 } TUPLE[FILES];
 
-int isNum(char *words){
-    if(words[0] >= '0' && words[0] <= '9')
+TUPLE tuples;
+
+// 判断是否为数字
+int isNum(char *word){
+    if (word[0] >= '0' && word[0] <= '9')
         return 1;
     return 0;
 }
 
-/* TODO
- * 余弦相似度计算函数 */
-double cosCalculate(){
-
+// 判断是否为关键字
+int isKeyword(char *word){
+    for (int i = 0; i < ALL_KEYWORD; i++)
+        if (!strcmp(word, KEYWORDS[i]))
+            return KEYWORD_CODE + i;
+    return 0;
 }
 
-/* TODO
- * 比较函数
- * 使用生成好的向量计算查重 */
-void compare(){
-
+// 判断是否为符号
+int isSymbol(char *word){
+    for(int i = 0; i < ALL_SYMBOL; i++)
+        if(!strcmp(word, SYMBOLS[i]))
+            return SYMBOL_CODE + i;
+    return 0;
 }
 
-/* TODO
- * 处理文件，将程序转换成向量 */
-void fileProcessor(FILE *fp){
-    int count = 0;
-    while(fgets(buffer, SEN_LEN, fp) != NULL){
-        word_A = strtok(buffer, " \t\n");
-        while(word_A != NULL){
-            printf("%s\n\n", word_A);
+// 判断是否为标识符
+int isIdentifier(char *word){
+    if(isalpha(word[0]) || word[0] == '_')
+        return IDEN_CODE;
+    return 0;
+}
 
-            word_B = word_A;
-            strchr(word_B, );
-            while(*word_B != '\0'){
+int isCommand(char *word){
+    for(int i = 0; i < ALL_COMMAND; i++)
+        if(!strcmp(word, COMMANDS[i]))
+            return COMMAND_CODE + i;
+    return 0;
+}
 
-                word_B++;
+// 处理文件，将程序分词，并将其转换成向量
+int fileProcessor(FILE *fp, int fileIndex){
+    int count = 0, code, inComment = 0, len; // count:记录已录入的词数; code:临时存储判断函数的值; inComment:用于标记是否在多行注释中
+    char *start, *ptr, symbol[2]; // start:记录数字和单词的起始地址; ptr:指针; symbol: 用于存储符号，后期会删掉
+
+    while (fgets(BUFFER, SEN_LEN, fp) != NULL) {
+        ptr = BUFFER; // 将指针指向此句句首
+        while (*ptr != '\0') {
+            if (!strncmp(ptr, "//", 2)) {// 跳过单行注释
+                break; // 跳过本行检查
+            }if(!strncmp(ptr, "/*", 2)){ // 检测到是多行注释开头，将inComment改为1
+                inComment = 1, ptr += 2;
+                continue; // 并跳过本次循环
+            }if(!strncmp(ptr, "*/", 2)){ // 检查到是多行注释终止符号，将inComment改成0
+                inComment = 0, ptr += 2;
+                continue; // 并跳过本次循环
+            }if(inComment){
+                ptr++;
+                continue;
+            } // 跳过多行注释
+
+            if(isalnum(*ptr) || *ptr == '_'){ // 提取数字和单词
+                start = ptr;
+                while(isalnum(*ptr) || *ptr == '_')
+                    ptr++;
+                len = ptr - start;
+                strncpy(WORD, start, len); // 讲单词和数字拷贝进单词缓冲区
+                WORD[len] = '\0';
+                // 用于截取数字或关键字或标识符
+                code = isKeyword(WORD);
+                if(!code && isNum(WORD))
+                    code = NUMBER_CODE; // 处理数字
+                if(!code)
+                    code = isCommand(WORD);
+                if(!code)
+                    code = isIdentifier(WORD); // 处理标识符
+                if(code){
+                    tuples[fileIndex].code[count] = code;
+                    strcpy(tuples[fileIndex].str[count++], WORD); // 处理关键字、标识符、预处理指令
+                }
+            }else{ // 处理符号
+                if(*ptr == '"'){
+                    ptr++;
+                    while(*ptr != '\"' || *ptr == '\"' && *(ptr - 1) == '\\')
+                        ptr++;
+                    tuples[fileIndex].code[count] = STRING_CODE;
+                    strcpy(tuples[fileIndex].str[count++], "STRING");
+                }else{
+                    symbol[0] = *ptr, symbol[1] = '\0';
+                    code = isSymbol(symbol);
+                    if(code){
+                        tuples[fileIndex].code[count] = code;
+                        strcpy(tuples[fileIndex].str[count++], symbol);
+                    }
+                }
+                ptr++;
             }
-
-            word_A = strtok(NULL, " \t\n");
-            count++;
         }
     }
+    return count;
 }
 
-int main(){
-    printf("**请将模板C语言程序放在data文件夹内，被查重的程序放在check文件夹内**\n");
+int main() {
+    printf("**请将模板C语言程序命名为 Data.c，被查重程序命名为 TestCode.c，然后将它们和本程序放在一起**\n");
     printf("**开始对数据进行处理**\n");
-    // 程序初始输出
-    FILE *fp = NULL;
-    fp = fopen(DATAFILE, "r");
+    // 程序开始
+    FILE *fp = fopen(DATAFILE, "r");
+    int count[FILES];
     if(fp != NULL){
-        fileProcessor(fp);
-        if(fclose(fp)){
-            perror("文件无法正常关闭！\n");
-            return EXIT_FAILURE;
-        }else
-            printf("文件已关闭\n");
+        count[1] = fileProcessor(fp, 1);
+        fclose(fp);
+        printf("文件已关闭\n");
     }else{
-        perror("文件打开失败，请检查文件是否正常或Data.c文件是否存在!\n");
+        perror("文件打开失败，请检查文件是否正常或 Data.c 文件是否存在!\n");
         return EXIT_FAILURE;
-    }
+    } // 对原程序进行处理
+
+    fp = fopen(CHECKFILE, "r");
+    if(fp != NULL){
+        count[0] = fileProcessor(fp, 0);
+        fclose(fp);
+        printf("文件已关闭\n");
+    }else{
+        perror("文件打开失败，请检查文件是否正常或 TestCode.c 文件是否存在!\n");
+        return EXIT_FAILURE;
+    } // 对被查重程序进行处理
+
+    for(int i = 0; i < FILES; i++){
+        printf("File: %s\n", !i ? CHECKFILE : DATAFILE);
+        for(int j = 0; j < MAX_LEN && tuples[i].code[j] != 0; j++)
+            printf("(%d, %s)\n", tuples[i].code[j], tuples[i].str[j]);
+    } // 输出提取的二元组，方便debug和查看效果
+
     return 0;
 }
